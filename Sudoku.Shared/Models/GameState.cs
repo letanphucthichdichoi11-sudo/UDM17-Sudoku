44db20e7691d493ba87111da2a95600191f9218e
letanphucthichdichoi11-sudo <letanphucthichdichoi11@gmail.com>
2026-08-11 11:25:44 +0700

    Implement GameState shared model

--- Sudoku.Shared/Models/GameState.cs
+++ Sudoku.Shared/Models/GameState.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+        
 namespace Sudoku.Shared.Models
 {
-    internal class GameState
+    public class GameState
     {
+        // ID của trận đấu
+        public Guid MatchId { get; set; }
+
+        // Bàn Sudoku hiện tại
+        // 0 = ô chưa có số
+        public int[,] Board { get; set; }
+
+        // Người chơi đang thực hiện lượt
+        public Guid CurrentPlayerId { get; set; }
+
+        // Trạng thái trận đấu
+        public GameStatus Status { get; set; }
+
+        // Khởi tạo GameState mặc định
+        public GameState()
+        {
+            MatchId = Guid.NewGuid();
+            Board = new int[9, 9];
+            CurrentPlayerId = Guid.Empty;
+            Status = GameStatus.Waiting;
+        }
+    }
+
+    public enum GameStatus
+    {
+        Waiting,
+        Playing,
+        Finished,
+        Aborted
     }
 }
